4b81cdffab5d62e346745f9238e5bc395e3df296
briannaordaz <briannaordaz5@gmail.com>
2025-04-29 16:39:39 -0700

    completed step 2, added models a models folder, and created files named "Student.cs", "Course.cs", and "Enrollment.cs".

---
+++ COMP003B.LectureActivity6/Models/Course.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity6.Models;
+
+public class Course
+{
+    public int CourseId { get; set; }
+    
+    [Required]
+    public string Title { get; set; }
+    
+    //Collection navigation property
+    public virtual ICollection<Enrollment>? Enrollments { get; set; }
+}
